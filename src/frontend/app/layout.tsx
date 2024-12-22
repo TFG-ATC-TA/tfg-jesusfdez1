32f4a600f4bc3ce3db3cd093c8ec5c4ba54c3a83
jesus.fdez1 <jesus.fdez1@alu.uclm.es>
2024-12-22 14:12:33 +0100

    Corrección código de layout.tsx
    Se corrige error del commit anterior en el layout.tsx

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -1,34 +1,4 @@
-<<<<<<< Updated upstream
-'use client'
 
-import { ScrollArea } from '@/components/ui/scroll-area'
-
-export default function AuthenticationPage() {
-  return (
-    <html>
-      <body>
-        <ScrollArea className="h-screen w-full">
-          <div className="relative min-h-screen flex items-center justify-center lg:justify-end bg-background">
-
-            <div className="relative z-10 w-full max-w-md mx-4 my-8 p-6 bg-background/95 dark:bg-background/90 rounded-lg shadow-xl lg:mx-0 lg:my-0 lg:mr-24">
-              <div className="flex items-center justify-center mb-6">
-                <span className="font-bold text-xl text-foreground">LactoKeeper</span>
-              </div>
-              
-              <div className="space-y-6">
-                <div className="text-center">
-                  <h1 className="text-2xl font-semibold tracking-tight text-foreground">
-                    Esto es una prueba hecha con NextJS
-                  </h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </ScrollArea>
-      </body>
-    </html>
-  )
-=======
 import Providers from '@/components/layout/providers';
 import { Toaster } from '@/components/ui/toaster';
 import type { Metadata } from 'next';
@@ -58,5 +28,4 @@ export default function RootLayout({
       </body>
     </html>
   );
->>>>>>> Stashed changes
 }
