429ad9c7c187fccce673fa4e876a558e30b246ec
jesus.fdez1 <jesus.fdez1@alu.uclm.es>
2025-01-01 20:22:14 +0100

    Incorporación de Header en el menú
    Añadido componente Header y su integración en el layout principal

--- src/frontend/app/(home)/layout.tsx
+++ src/frontend/app/(home)/layout.tsx
@@ -3,6 +3,7 @@
 import { useSession } from "next-auth/react"
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
+import Header from '@/components/layout/header'
 import Sidebar from '@/components/layout/sidebar'
 import { MobileSidebar } from '@/components/layout/mobile-sidebar'
 import { cn } from '@/lib/utils';
@@ -31,6 +32,7 @@ export default function DashboardLayout({
         <Sidebar />
       </div>
       <main className="w-full flex-1 overflow-hidden p-5 sm:p-0 lg:mb-0 lg:mx-4 md:mx-4">
+        <Header />
         {children}
       </main>
       <div className={cn('lg:hidden z-50')}>
