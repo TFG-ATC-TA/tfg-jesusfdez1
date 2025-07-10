d0e354717ae45143326b8244244e44b675f9cba2
jesus.fdez1 <jesus.fdez1@alu.uclm.es>
2025-07-10 18:33:29 +0200

    Mejoras en Docker
    Configurar variables de entorno y agregar nuevos scripts de construcción y ejecución en Docker

--- src/frontend/next.config.mjs
+++ src/frontend/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {   
     reactStrictMode: true,
+    // Configuración para variables de entorno
+    env: {
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        INTERNAL_API_URL: process.env.INTERNAL_API_URL,
+    }
+    // Configuración para desarrollo y producción
+
 //    eslint: {
 //        ignoreDuringBuilds: true,
 //    },
